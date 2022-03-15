333c6b578fb345b1ba6666c43d8230fa66ffdcb0
Lawreros <lawreros@gmail.com>
2022-03-15 17:42:47 -0400

    add file preview for RR-intervals

--- GUI_test.m
+++ GUI_test.m
@@ -18,19 +18,29 @@ function PSHR_GUI
     ofig = uifigure('Name','PSHR Analysis Pipeline','Position', [0 100 1000 600]);
     
     %Display Loaded files
+    viewselect={};
     uilabel(ofig, 'Position', [20 570 200 20], 'Text', 'Loaded HR Files');
-    hr_load_list = uilistbox(ofig, 'Position', [20 460 300 100],...
-        'Items', {'None'}, 'Multiselect','on');%,...
-        %'ValueChangedFcn', @(src,event));
+    hr_load_list = uilistbox(ofig, 'Position', [20 480 300 80],...
+        'Items', {'None'}, 'Multiselect','on',...
+        'ValueChangedFcn', @(src,event)disp(src.Value));
     
     uilabel(ofig, 'Position', [420 570 200 20], 'Text', 'Loaded ECG Files');
-    ecg_load_list = uilistbox(ofig, 'Position', [340 460 300 100],...
-        'Items', {'None'}, 'Multiselect','on');
+    ecg_load_list = uilistbox(ofig, 'Position', [340 480 300 80],...
+        'Items', {'None'}, 'Multiselect','on',...
+        'ValueChangedFcn', @(src,event)disp(src.Value));
     
     
     uilabel(ofig, 'Position', [720 570 200 20], 'Text', 'Loaded Affect Files');
-    aff_load_list = uilistbox(ofig, 'Position', [660 460 300 100],...
+    aff_load_list = uilistbox(ofig, 'Position', [660 480 300 80],...
         'Items', {'None'}, 'Multiselect', 'on');
+    
+    
+    %View Loaded files Raw Data
+    uibutton(ofig,'Position',[20 440 100 22],'Text','View',...
+         'ButtonPushedFcn', @(src,event)line_plot(hr_load_list.Value,'HR','Raw'));
+    
+    uibutton(ofig,'Position',[340 440 100 22],'Text','View',...
+         'ButtonPushedFcn', @(src,event)line_plot(ecg_load_list.Value,'ECG','Raw'));
     
     
     
@@ -108,11 +118,16 @@ function PSHR_GUI
     
     %Load Selected Files by User
     function LoadSelected(type)
+        
+        %Clear previously loaded information
+        clear Data;
+        Data.HR.Raw{1} = {};
+        Data.ECG.Raw{1} = {};
+        
         switch type
             case 'hr'
                 [file, path] = uigetfile('*.txt','MultiSelect','on');
                 Data.HR.path = path;
-                Data.HR.files = file;
                 
                 if iscell(file)
                     for i = 1:length(file)
@@ -120,19 +135,20 @@ function PSHR_GUI
                         Data.HR.Raw{i} = vectorize(dump);
                         clear dump;
                     end
+                    Data.HR.files = file;
                     hr_load_list.Items=file;
                 else
                     dump = data_load(strcat(path,file));
                     Data.HR.Raw = vectorize(dump);
                     clear dump;
                     %Display the files that are loaded
+                    Data.HR.files = {file};
                     hr_load_list.Items={file};
                 end
                 
             case 'ecg'
                 [file,path] = uigetfile('*.txt','MultiSelect','on');
                 Data.ECG.path = path;
-                Data.ECG.files = file;
                 
                 if iscell(file)
                     for i = 1:length(file)
@@ -140,20 +156,73 @@ function PSHR_GUI
                         Data.ECG.Raw{i} = vectorize(dump);
                         clear dump;                        
                     end
+                    Data.ECG.files = file;
                     ecg_load_list.Items=file;
                 else
                     dump = data_load(strcat(path,file));
                     Data.ECG.Raw = vectorize(dump);
                     clear dump;
+                    Data.ECG.files = {file};
                     ecg_load_list.Items={file};
                 end
                 
         end
         disp(strcat('Loading file: ', path, file));
     end
-    
+
+    function line_plot(files, type, struct)
+        %files : txt files that you want to use
+        %type : 'HR' or 'ECG'
+        %struct : the name of the structure file you want to use
+        
+        %Plots what is in Data.<type>.<struct>
+        
+        [entries] = entry_select(Data.(type).files, files);
+        if length(entries) > 1
+            
+            for i=1:length(entries)
+                plot(Data.(type).(struct){i}(:,3));
+                hold on;
+            end
+            hold off;
+        else
+            %Address annoying fact that if only one file is loaded, then
+            %structures can stop being cell arrays
+            if iscell(Data.(type).(struct))
+                plot(Data.(type).(struct){entries}(:,3));
+                hold off;
+            else
+                plot(Data.(type).(struct)(:,3));
+            end
+        end
+    end
+
 end
 
+function [entries] = entry_select(list, target)
+    %Given a list of strings and set of target(s), returns the entry number
+    %in the list of each of the targets.
+    
+    %list : cell array of strings
+    %target : cell array of strings or single string
+    %entries : matrix vector containing the entry numbers
+    
+    %create cell array if target is single string
+    
+    disp(target);
+    disp(list);
+    if iscell(target) == 0
+        target = {target};
+    end
+    
+    if iscell(list) == 0
+        list = {list};
+    end
+    
+    matches = ismember(list,target);
+    entries = find(matches);
+
+end
 
 function [raw_array] = data_load(fpath)
     %Load in file
@@ -180,7 +249,6 @@ function [raw_array] = data_load(fpath)
     disp(strcat(fpath, ': LOADED'));
     raw_array = cell2mat(raw_array);
 end
-
 
 function [new_array] = vectorize(matrix_array)
     %Take the raw array and concatonate all of the data entries into a
