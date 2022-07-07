f95be70ab600fc834926c54cc7130037f8c14415
Lawreros <lawreros@gmail.com>
2022-07-07 13:14:29 -0400

    add functionality for affect comparison of multiple files

--- coding_comp.m
+++ coding_comp.m
@@ -3,119 +3,179 @@
 %different people
 clear all;
 
-aff_1 = 'ignore_samples/Chat&Chew_2022-04-29_1255_V01_Montanez.csv';
-aff_2 = 'ignore_samples/Chat&Chew_2022-04-29_1255_V01_Kessler.csv';
+inp_dir = 'Reliability_Training/';
+
+aff_1 = 'Chat&Chew_2022-04-29_1255_V01_ANSWER_KEY.csv';
+aff_2 = {'Chat&Chew_2022-04-29_1255_V01_Liu.csv',...
+    'Chat&Chew_2022-04-29_1255_V01_Baldie1.csv',...
+    'Chat&Chew_2022-04-29_1255_V01_Zhao.csv'};
+out_file = 'test_comp.xlsx';
 
 
-tab_1 = readtable(aff_1, 'Format', 'auto');
-tab_2 = readtable(aff_2, 'Format', 'auto');
-
-% Change column names so you don't have duplicate issues
-tab_2.Properties.VariableNames{2} = 'Affect1_2';
-tab_2.Properties.VariableNames{3} = 'Affect2_2';
-tab_2.Properties.VariableNames{4} = 'Affect3_2';
-tab_2.Properties.VariableNames{17} = 'on_camera_2'; %TODO: This is for the new coding format, will be wrong for 03-18 coding
-tab_2.Properties.VariableNames{18} = 'problem_yn_2'; % ""
-
-%Make table for comparison
-comp_tab = tab_1(:,[1:4,17,18]);%25,26]);
-comp_tab = [comp_tab, tab_2(:,[2:4,17,18])];%25,26])];
-
-%free up some memory
-clear tab_1 tab_2;
+for z = 1:length(aff_2)
 
 
-%Begin comparison iterations
-[r,c] = size(comp_tab);
+    tab_1 = readtable(strcat(inp_dir,aff_1), 'Format', 'auto');
+    tab_2 = readtable(strcat(inp_dir,aff_2{z}), 'Format', 'auto');
 
-comp_index = {'Disagreement',aff_2;aff_1,0};
-comp_agree = {'Affect'};
+    % Change column names so you don't have duplicate issues
+    tab_2.Properties.VariableNames{2} = 'Affect1_2';
+    tab_2.Properties.VariableNames{3} = 'Affect2_2';
+    tab_2.Properties.VariableNames{4} = 'Affect3_2';
+    tab_2.Properties.VariableNames{17} = 'on_camera_2'; %TODO: This is for the new coding format, will be wrong for 03-18 coding
+    tab_2.Properties.VariableNames{18} = 'problem_yn_2'; % ""
 
-for i = 1:r
-    comp_1 = table2cell(comp_tab(i,2:4));
-    comp_1 = comp_1(~isempty(comp_1));
-    comp_2 = table2cell(comp_tab(i,7:9));
-    comp_2 = comp_2(~isempty(comp_2));
+    %Make table for comparison
+    comp_tab = tab_1(:,[1:4,17,18]);%25,26]);
+    comp_tab = [comp_tab, tab_2(:,[2:4,17,18])];%25,26])];
+
+    %free up some memory
+    clear tab_1 tab_2;
+
+
+    %Begin comparison iterations
+    [r,c] = size(comp_tab);
+
+    comp_index = {'Disagreement',aff_2{z};aff_1,0};
+    comp_agree = {'Affect'};
+
+    for i = 1:r
+        comp_1 = table2cell(comp_tab(i,2:4));
+        comp_1 = comp_1(~isempty(comp_1));
+        comp_2 = table2cell(comp_tab(i,7:9));
+        comp_2 = comp_2(~isempty(comp_2));
+
+        qq = comp_1(~ismember(comp_1, comp_2));
+        zz = comp_2(~ismember(comp_2, comp_1));
+
+        %Create list of agreements
+        pp = unique([comp_1(ismember(comp_1, comp_2)),comp_2(ismember(comp_2, comp_1))]);
+
+        for q = 1:length(pp)
+            if ismember(pp{q},comp_agree(:,1))
+                j = find(ismember(comp_agree(:,1),pp{q}));
+                comp_agree{j,2} = comp_agree{j,2}+1;
+            else
+                comp_agree{end+1,1} = pp{q};
+                comp_agree{end,2} = 1;
+            end
+
+        end
+
+        if ~isempty(qq) || ~isempty(zz) %If there is a disagreement
+            disp(strcat('Disagreement found at time:', string(comp_tab{i,1})));
+
+            if isempty(qq)
+                b = strjoin(zz,'&');
+                a = 'n/a';
+            elseif isempty(zz)
+                b = 'n/a';
+                a = strjoin(qq, '&');
+            else % Both groups have a unique affect
+                b = strjoin(zz,'&');
+                a = strjoin(qq,'&');
+            end
+
+            %key = strcat(a, '_vs_', b);
+
+            %Add to the array
+
+            if ismember(a,comp_index(:,1)) && ismember(b,comp_index(1,:))
+                j = find(ismember(comp_index(:,1),a));
+                k = find(ismember(comp_index(1,:),b));
+                comp_index{j,k} = [comp_index{j,k},comp_tab{i,1}];
+
+            elseif ismember(a,comp_index(:,1)) && ~ismember(b,comp_index(1,:)) %only a already exists
+                j = find(ismember(comp_index(:,1),a));
+                comp_index{1,end+1} = b; %add the b category
+                comp_index{j,end} = comp_tab{i,1};
+
+            elseif ismember(b,comp_index(1,:)) %only b already exists
+                k = find(ismember(comp_index(1,:),b));
+                comp_index{end+1,1} = a;
+                comp_index{end,k} = comp_tab{i,1};
+
+            else %neither exist
+                comp_index{end+1,1} = a;
+                comp_index{1,end+1} = b;
+                comp_index{end,end} = comp_tab{i,1};
+
+            end
+
+        end
+
+    end
+
+    % Convert the comp_index to cells with strings so they can be saved as
+    % excell files
+
+    bb = comp_index;
+    cc = comp_index;
+    [r,c] = size(comp_index);
+    for i = 3:r
+        for j = 3:c
+            bb{i,j} = strjoin(string(bb{i,j}),', ');
+            cc{i,j} = length(comp_index{i,j});
+        end
+    end
     
-    qq = comp_1(~ismember(comp_1, comp_2));
-    zz = comp_2(~ismember(comp_2, comp_1));
-    
-    %Create list of agreements
-    pp = unique([comp_1(ismember(comp_1, comp_2)),comp_2(ismember(comp_2, comp_1))]);
-        
-    for q = 1:length(pp)
-        if ismember(pp{q},comp_agree(:,1))
-            j = find(ismember(comp_agree(:,1),pp{q}));
-            comp_agree{j,2} = comp_agree{j,2}+1;
+
+    % Add the seconds of disagreement to comp_agree for easy exporting
+    aff_list = unique([transpose(comp_index(3:end,1)), comp_index(1,3:end)]);
+
+    for i = 1:length(aff_list)
+        c = find(ismember(comp_index(1,:),aff_list{i}));
+        r = find(ismember(comp_index(:,1),aff_list{i}));
+
+        count = 0;
+
+        if r
+            count = count + sum(cellfun('length',comp_index(r,3:end)));
+        end
+
+        if c
+            count = count + sum(cellfun('length',comp_index(3:end, c)));
+        end
+
+        if isempty(r)==0 && isempty(c)==0
+            count = count - cellfun('length',comp_index(r,c));
+        end
+
+        % Append to comp_agree or make new row
+        r = find(ismember(comp_agree(:,1), aff_list{i}));
+        if r
+            comp_agree{r,3} = count;
         else
-            comp_agree{end+1,1} = pp{q};
-            comp_agree{end,2} = 1;
+            comp_agree{end+1,1} = aff_list{i};
+            comp_agree{end,2} = 0;
+            comp_agree{end,3} = count;
         end
-        
     end
-        
-    if ~isempty(qq) || ~isempty(zz) %If there is a disagreement
-        disp(strcat('Disagreement found at time:', string(comp_tab{i,1})));
-        
-        if isempty(qq)
-            b = strjoin(zz,'&');
-            a = 'n/a';
-        elseif isempty(zz)
-            b = 'n/a';
-            a = strjoin(qq, '&');
-        else % Both groups have a unique affect
-            b = strjoin(zz,'&');
-            a = strjoin(qq,'&');
-        end
-        
-        %key = strcat(a, '_vs_', b);
-        
-        %Add to the array
-        
-        if ismember(a,comp_index(:,1)) && ismember(b,comp_index(1,:))
-            j = find(ismember(comp_index(:,1),a));
-            k = find(ismember(comp_index(1,:),b));
-            comp_index{j,k} = [comp_index{j,k},comp_tab{i,1}];
-            
-        elseif ismember(a,comp_index(:,1)) && ~ismember(b,comp_index(1,:)) %only a already exists
-            j = find(ismember(comp_index(:,1),a));
-            comp_index{1,end+1} = b; %add the b category
-            comp_index{j,end} = comp_tab{i,1};
-        
-        elseif ismember(b,comp_index(1,:)) %only b already exists
-            k = find(ismember(comp_index(1,:),b));
-            comp_index{end+1,1} = a;
-            comp_index{end,k} = comp_tab{i,1};
-            
-        else %neither exist
-            comp_index{end+1,1} = a;
-            comp_index{1,end+1} = b;
-            comp_index{end,end} = comp_tab{i,1};
-            
-        end
-        
+
+    % Calculate agreement rate for total problematic behavior
+    comp_agree{end+1,1} = "Existence of Problem Behavior";
+    comp_agree{end,3} = sum(abs(comp_tab.problem_yn - comp_tab.problem_yn_2));
+    comp_agree{end,2} = height(comp_tab) - comp_agree{end,3};
+
+    % Calculate rates of agreement
+    comp_agree{1,4} = "Agreement Rate";
+    comp_agree{1,2} = "Seconds of Agreement";
+    comp_agree{1,3} = "Seconds of Disagreement";
+    for i = 2:length(comp_agree)
+        comp_agree{i,4} = comp_agree{i,2} / (comp_agree{i,2}+comp_agree{i,3});
     end
     
-end
-
-% Convert the comp_index to cells with strings so they can be saved as
-% excell files
-
-bb = comp_index;
-[r,c] = size(comp_index);
-for i = 3:r
-    for j = 3:c
-        bb{i,j} = strjoin(string(bb{i,j}),',');
+    
+    % Save information into excell file
+    if ischar(out_file)
+        writecell(cc, out_file, 'Sheet', z, 'Range', 'A2:Z15')
+        writecell(bb, out_file, 'Sheet', z, 'Range', 'A16:Z29')
+        writecell(comp_agree, out_file, 'Sheet',z,'Range','A30:Z50');
     end
+        
+clear comp_agree comp_tab comp_tab bb;   
 end
-
-
-%Pad out and organize comp_index so that it has the same number of affects
-%for both reviewers
-
-unique([transpose(comp_index(3:end,1)), comp_index(1,3:end)]);
-
-
+    
 %Plot agreement and disagreement histogram
 
 
